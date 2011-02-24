b8c1b6c7fe2a6e7bcc77e4c1d17b020a68a2c1f0
Juan Manuel Barreneche <snipperme@gmail.com>
2011-02-23 22:14:21 -0300

    Added running example Run as: ruby -Ilib example/canvas.rb

--- example/canvas.rb
+++ example/canvas.rb
@@ -1,5 +1,8 @@
 require 'bin'
 
+# To run the example make sure the bin project is in the load_path
+# you can run it from the project root like:
+# ruby -Ilib example/canvas.rb
 class Canvas
 
   attr_reader :store
@@ -31,3 +34,56 @@ class Canvas
     drawings.empty?
   end
 end
+
+puts "It's time to paint!"
+puts 
+
+# grab a new canvas
+canvas = Canvas.new(640, 480)
+
+canvas.draw_rectangle(  0..320,   0..240, :blue)
+canvas.draw_rectangle(321..640,   0..240, :red)
+canvas.draw_rectangle(  0..320, 241..480, :yellow)
+canvas.draw_rectangle(321..640, 241..480, :green)
+
+canvas.draw_rectangle(100..540, 100..380, :black)
+
+puts <<CANVAS
+  Canvas: 640 x 480
+
+  5 rectangles
+  _________________
+  |Yellow | Green |
+  |    ___|___    |
+  |___|_Black_|___|
+  |   |___|___|   |
+  | Blue  | Red   |
+  |_______|_______|
+
+CANVAS
+
+puts "Colors in the center:"
+canvas.drawings(320..321, 240..241).each do |rectangle|
+  puts "- #{rectangle.value}"
+end
+puts 
+
+puts "Colors in the right half:"
+canvas.drawings(321..640, 0..480).each do |rectangle|
+  puts "- #{rectangle.value}"
+end
+puts 
+
+puts "Colors in the top half:"
+canvas.drawings(0..640, 241..480).each do |rectangle|
+  puts "- #{rectangle.value}"
+end
+puts 
+
+puts "Cleanining up the canvas..."
+canvas.drawings.each do |rectangle|
+  canvas.erase_drawing(rectangle)
+end
+puts 
+
+puts "Now the canvas is empty!" if canvas.empty?
